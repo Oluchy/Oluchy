bf885fd17694901da5016bfb30c5f10a663fc456
Oluchy <61972145+Oluchy@users.noreply.github.com>
2020-03-26 00:17:28 +0100

    Update Task.js

--- Task.js
+++ Task.js
@@ -1,5 +1,5 @@
 let myName = "Margaret Oluchi Eze";
 console.log("My name is "   + myName);
-let courses = ["Courses:"+"html","css","Javascrit","UI/UX design"];
+let courses = ["Courses:"+"HTML","CSS","JavaScript","UI/UX Design"];
 console.log(courses);
 for (let i = 0; i <= 200; i++) {if (i % 2 !== 0) console.log(i) }
