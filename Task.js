05b5d3802d7b202be1d8c67c8dd86047be4d3e56
Oluchy <61972145+Oluchy@users.noreply.github.com>
2020-03-25 23:44:59 +0100

    Add files via upload

---
+++ Task.js
@@ -0,0 +1,5 @@
+let myName = "Margaret Oluchi Eze";
+console.log("My name is "   + myName);
+let courses = ["Courses:"+"html","css","Javascrit","UI/UX design"];
+console.log(courses);
+for (let i = 0; i <= 200; i++) {if (i % 2 !== 0) console.log(i) }
